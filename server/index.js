8eb5aa2d4c0fbe8e024587e03b3f910972d9b8b8
devacunetixtech <adebisiprecious780@gmail.com>
2023-09-15 02:58:55 +0100

    bbase url again

--- server/index.js
+++ server/index.js
@@ -7,10 +7,8 @@ const app = express();
 require("dotenv").config()
 
 app.use(express.json());    
-app.use(cors({
-    origin:["https://localhost:3000", "https://netix-pay-prot.onrender.com/api/users"],
-}));
-// app.use("/api/users", userRoute);
+app.use(cors());
+app.use("/api/users", userRoute);
 
 app.get("/", (req, res)=>{
     res.send("Welcome to MERN STACK")
